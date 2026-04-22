4a141547dc1501920a94c1787e9b72a267fccb0b
espoirbaraka <esbarakabigega@gmail.com>
2026-04-22 11:20:20 +0200

    Database

---
+++ core/Database.php
@@ -0,0 +1,9 @@
+<?php
+class Database{
+    public $connexion;
+
+    public function connect(){
+       $connexion = new PDO('mysql:host=localhost;dbname=hopital', 'root', ''); 
+       return $connexion;
+    }
+}
---
+++ core/Model.php
@@ -0,0 +1,9 @@
+<?php
+class Model{
+    public $db;
+
+    public function __construct(){
+        $database = new Database();
+        return $this->db = $database->connect();
+    }
+}
